1c6f5acbe08ac4111276969d21936e6ff64a86ca
Muhammad Hamza <frndbros@gmail.com>
2024-11-15 19:33:59 +0530

    updated cros

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://lemida-lms.vercel.app/";
+export const server = "https://lemida-lms.vercel.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
