f24f495b1680a2a35b1fd006143fd6a413ca7e02
Muhammad Hamza <frndbros@gmail.com>
2024-11-15 17:57:29 +0530

    init try2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:8001";
+export const server = process.env.MainDomainName;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
