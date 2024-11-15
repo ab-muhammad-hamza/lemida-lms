279537a6495d932f44483b3a708ddb1253116bb4
Muhammad Hamza <frndbros@gmail.com>
2024-11-15 19:43:52 +0530

    mongo

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.DB);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database Connected");
   } catch (error) {
     console.log(error);
