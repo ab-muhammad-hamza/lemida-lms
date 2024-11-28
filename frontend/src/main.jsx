ba1d77cc20dc0a7596ef9daf69936edf7c657532
Muhammad Hamza <frndbros@gmail.com>
2024-11-28 16:25:56 +0530

    Server Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import dotenv from "dotenv";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:8001";
+export const server = process.env.MainDomainName;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
