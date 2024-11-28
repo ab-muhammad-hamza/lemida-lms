9f9a2179eb95179f29bd52d09ed7b192d1d2217c
Muhammad Hamza <frndbros@gmail.com>
2024-11-28 17:04:30 +0530

    will this work?

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = process.env.MainDomainName;
+export const server = "https://lemida-lms.vercel.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
